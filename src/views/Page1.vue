aae3a474fd90fb1272875432c1888af6dcd37e26
merci10 <yka_oct10@yahoo.co.jp>
2019-03-08 01:30:24 +0900

    Page1 addボタン押したらカウントアップするように実装 Page2 修正

--- src/views/Page1.vue
+++ src/views/Page1.vue
@@ -1,7 +1,51 @@
 <template>
-	<div class="page1">
-		<h1>This is a sample Page1</h1>
-	</div>
+  <div class="page1">
+    <h1>This is a sample Page1</h1>
+    <h2>Shopping Cart Example</h2>
+    <div>----------------------------------------------------------------</div>
+    <h2>Products</h2>
+    <ul>
+      <li>
+        iPhoneXR<br>
+        <button type="button" @click="countUp()">add</button>
+      </li>
+    </ul>
+    <div>----------------------------------------------------------------</div>
+    <ul>
+      <li v-if="count > 0">
+        iPhoneXR x{{ count }}
+      </li>
+    </ul>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    countUp() {
+      this.count++;
+    }
+  }
+};
+</script>
 
+<style scoped>
+ul {
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  list-style-type: disc;
+}
+
+li::before {
+  content: '・';
+  display: inline-block;
+}
+</style>
--- src/views/Page2.vue
+++ src/views/Page2.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="page2">
-      <h1 v-if="title.lenth > 0">
-        {{ title }}
-      </h1>
-      <h1 v-else>
-        Title is empty...
-      </h1>
-    <input type="text" v-model="title">
+    <h1 v-if="title.length > 0">
+      {{ title }}
+    </h1>
+    <h1 v-else>
+      Title is empty...
+    </h1>
+    <input type="text" v-model="title" />
     <button type="button" @click="clear()">clear</button>
   </div>
 </template>
@@ -15,13 +15,13 @@
 export default {
   data() {
     return {
-      title: 'This is a sample Page2'
-    }
+      title: "This is a sample Page2"
+    };
   },
-   methods: {
+  methods: {
     clear() {
-      this.title = ''
+      this.title = "";
     }
   }
-}
+};
 </script>
